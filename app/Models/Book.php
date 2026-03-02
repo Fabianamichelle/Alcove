b69e956f5a7cd9f73a1075e428e5246c622d3edc
fabiana <fabianamichellee@gmail.com>
2026-03-02 17:34:47 -0600

    Add logic for model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    // These are the only fields we allow to be mass-assigned
+    protected $fillable = ['title', 'author', 'total_pages', 'current_page', 'notes'];
+
+    // This links the book back to you (the Owner)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

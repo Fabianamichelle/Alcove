6c3f1536ed744ce1cf0902872359243b0522e943
fabiana <fabianamichellee@gmail.com>
2026-03-14 11:17:10 -0600

    Add userid to model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     // These are the only fields we allow to be mass-assigned
-    protected $fillable = ['title', 'author', 'total_pages', 'current_page', 'notes'];
+    protected $fillable = ['title', 'author', 'total_pages', 'current_page', 'notes', 'user_id'];
 
     // This links the book back to you (the Owner)
     public function user()

8f837b4eac027d38663a1ead1897bdbe391b1a13
fabiana <fabianamichellee@gmail.com>
2026-03-15 04:57:50 -0600

    Add Logo

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,8 @@
 
 <title>Alcove</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ asset('Alcove.png') }}?v=2" type="image/png">
+<link rel="apple-touch-icon" href="{{ asset('Alcove.png') }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
